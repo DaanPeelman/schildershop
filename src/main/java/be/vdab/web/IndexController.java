a183acb9e7ad17baf0a2975c48e6a38b7b26d81f
Daan <daanpeelman@gmail.com>
2014-08-18 09:49:54 +0200

    css stijl van laatste producten aangepast

--- src/main/java/be/vdab/web/IndexController.java
+++ src/main/java/be/vdab/web/IndexController.java
@@ -1,27 +1,50 @@
 package be.vdab.web;
 
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.servlet.ServletContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import be.vdab.entities.Product;
 import be.vdab.services.ProductService;
 
 @Controller
 @RequestMapping(value = "/")
 public class IndexController {
+	private final ServletContext servletContext;
+	
 	private final ProductService productService;
 	
 	@Autowired
-	public IndexController(ProductService productService) {
+	public IndexController(ProductService productService, ServletContext servletContext) {
 		this.productService = productService;
+		this.servletContext = servletContext;
 	}
 
-	@RequestMapping
+	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView index() {
+		Map<Product, Boolean> mapProducten = new ConcurrentHashMap<>();
+		Iterable<Product> producten = productService.findNieuwsteVijfProducten();
+		
+		for(Product product:producten) {
+			String productFotoPad = servletContext.getRealPath("/img") + "\\" + product.getProductId() + ".jpg";
+			File file = new File(productFotoPad);
+			mapProducten.put(product, file.exists());
+		}
+		
+		System.out.println("aantal producten in map: " + mapProducten.size());
+		
 		ModelAndView modelAndView = new ModelAndView("index");
-		modelAndView.addObject("laatsteProducten", productService.findNieuwsteVijfProducten());
+		modelAndView.addObject("laatsteProducten", mapProducten);
 		
 		return modelAndView;
 	}
