3815476839e04cb37b7980f43717c7bb7087dd01
LisaS <LisaS@192.168.1.4>
2014-09-05 20:22:15 +0200

    verwijderen ongebruikte imports

--- src/main/java/be/vdab/web/BestelbonController.java
+++ src/main/java/be/vdab/web/BestelbonController.java
@@ -2,11 +2,8 @@ package be.vdab.web;
 
 import java.security.Principal;
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
--- src/main/java/be/vdab/web/MandjeController.java
+++ src/main/java/be/vdab/web/MandjeController.java
@@ -13,7 +13,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import be.vdab.dao.GebruikerDAO;
 import be.vdab.entities.*;
 import be.vdab.services.GebruikerService;
 import be.vdab.services.ProductService;
